6b44f406d3298a5b145fa274a3e9e5a1f9a8db3a
Andrio Celos <AndrioCelos@users.noreply.github.com>
2016-01-10 15:56:04 +1100

    Tick version numbers.

--- Plugins/Games/UNO/Properties/AssemblyInfo.cs
+++ Plugins/Games/UNO/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Andrio Celos")]
 [assembly: AssemblyProduct("UNO")]
-[assembly: AssemblyCopyright("2012-5 Andrio Celos")]
+[assembly: AssemblyCopyright("2012-6 Andrio Celos")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.10.1.0")]
-[assembly: AssemblyFileVersion("1.10.1.0")]
+[assembly: AssemblyVersion("1.11.0.0")]
+[assembly: AssemblyFileVersion("1.11.0.0")]
